d74ecbcfaf68cd7671eabf04211acff923a3a77d
Jesse Rafalko <Jesse.Rafalko@gmail.com>
2016-07-26 14:41:25 -0400

    Phoenix on Travis

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,20 +4,35 @@ defmodule Peepchat.User do
   schema "users" do
     field :email, :string
     field :password_hash, :string
+
+    # Two virtual fields for password confirmation
     field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
 
-    timestamps()
+    timestamps
   end
-  
-  @required_fields ~w(name email bio number_of_pets)
+
+  @required_fields ~w(email password password_confirmation)
   @optional_fields ~w()
 
   @doc """
-  Builds a changeset based on the `struct` and `params`.
+  Creates a changeset based on the `model` and `params`.
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
+  def changeset(model, params \\ :empty) do
+    model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
+    |> hash_password
+    |> unique_constraint(:email) 
+  end
+
+  defp hash_password(%{valid?: false} = changeset), do: changeset
+  defp hash_password(%{valid?: true} = changeset) do
+      hashedpw = Comeonin.Bcrypt.hashpwsalt(Ecto.Changeset.get_field(changeset, :password))
+      Ecto.Changeset.put_change(changeset, :password_hash, hashedpw)
   end
 end
